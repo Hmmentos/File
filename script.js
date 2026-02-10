94c71001d0dbfb9a374060a21fff621735f55915
Hmmentos <genesisotnerolf@gmail.com>
2026-02-10 22:18:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function openFolder(folder, link) {
-  const id = folder.querySelector("p").innerText;
-  localStorage.setItem(id, "opened");
-
   folder.classList.add("used");
 
   setTimeout(() => {
@@ -9,11 +6,4 @@ function openFolder(folder, link) {
   }, 300);
 }
 
-window.onload = () => {
-  document.querySelectorAll(".folder").forEach(folder => {
-    const id = folder.querySelector("p").innerText;
-    if (localStorage.getItem(id)) {
-      folder.classList.add("used");
-    }
-  });
-};
+
